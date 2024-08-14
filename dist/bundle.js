59eba969998e9208bafddeab36b8e8fecd018f55
cuimm <bestcmm@163.com>
2024-08-14 11:42:22 +0800

    TS环境搭建

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+(function () {
+	'use strict';
+
+	const a = 100;
+	console.log(a);
+
+})();
+//# sourceMappingURL=bundle.js.map
---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ts from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    input: "src/index.ts",
+    output: {
+        file: path.resolve(__dirname, 'dist/bundle.js'),
+        sourcemap: true,
+        format: 'iife'
+    },
+    plugins: [
+        nodeResolve({
+            extensions: ['.js', '.ts']
+        }),
+        ts({
+            tsconfig: path.resolve(__dirname, 'tsconfig.json')
+        }),
+        serve({
+            open: true,
+            openPage: '/public/index.html',
+            port: 3000
+        }),
+    ]
+};
+
